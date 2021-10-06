46e1bac18fbe40d39f6538f2c6ddad900cf27109
ottomagnusson <ottomagnusson94@gmail.com>
2021-10-06 18:37:07 +0200

    Version 1.0 of package

--- R/decode_qualities.R
+++ R/decode_qualities.R
@@ -1,12 +1,22 @@
 ########################### decode_qualities function ##########################
 
+#' Title
+#'
+#' @param qualities  String of sequence Qualities from a fastq format file
+#' @param offset Scalar number which is either 33 or 64, the only two offsets used in fastq files. Default value is 33
+#'
+#' @return Vector of phred scores for each character of the sequence qualities, values cannot be < 0
+#' @export
+#'
+#' @examples decode_qualities("???#;ABAAAH")
+
 decode_qualities <- function(qualities, offset = 33) {
 
   assertthat::assert_that(assertthat::is.scalar(offset))
   assertthat::assert_that(is.numeric(offset))
 
   if (!(offset == 33 | offset == 64)) {
-    stop("Offset can only be 33 or 64")
+    base::stop("Offset can only be 33 or 64")
   }
 
   as.integer(charToRaw(qualities)) -> ascii_values
@@ -14,7 +24,7 @@ decode_qualities <- function(qualities, offset = 33) {
   ascii_values - offset -> phred_score
 
   if (any(phred_score < 1)) {
-    stop("Some Phred scores are less than 0")
+    base::stop("Some Phred scores are less than 0")
   }
 
   return(phred_score)
---
+++ R/help.R
@@ -0,0 +1,15 @@
+#' fastqR: A package for reading and manipulating fastq format sequence files
+#'
+#' The fastqR package provides three main functions which you might
+#' want to use.
+#'
+#' @section fastqR functions:
+#' read_fastq
+#'
+#' gc_content
+#'
+#' decode_qualities
+#'
+#' @docType package
+#' @name fastqR
+NULL
